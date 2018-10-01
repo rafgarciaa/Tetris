c53b4be523e3604890debbe585ec6975af0e50e2
Rafael Garcia <rafgarciaa@gmail.com>
2018-10-01 17:19:58 -0400

    Change background, remove start button

--- js/tetris.js
+++ js/tetris.js
@@ -1,5 +1,12 @@
 import Board from './board.js';
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('tetris');
   const ctx = canvas.getContext('2d');
@@ -10,22 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
   ctx.font = "1px serif";
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
-  ctx.fillText('Press Start to Play', (canvas.width/30)/2, (canvas.width/30)/2);
+  ctx.fillText('Press Enter to Play', (canvas.width/30)/2, (canvas.width/30)/2);
 
-  // debugger
   let gameRun = false;
   let board = new Board(canvas.width, canvas.height, ctx);
 
-  // selecting the button
-  const button = document.getElementById('start_game');
-  button.onclick = () => {
-    gameRun = true;
+  document.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+      gameRun = true;
+    }
 
     if (gameRun) {
       board.updateBoard(); // re-renders the grid
       board.updateScore();
     }
-  };
+  });
 
   // for testing purposes only START
   window.board = board;
